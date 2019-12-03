3525e4fc008b001a833f145335082ff8873b8d19
pagetronic <pagetronic@gmail.com>
2019-12-03 12:53:33 +0400

    debug

--- AgroneoWeb/html/gaia/gaia.js
+++ AgroneoWeb/html/gaia/gaia.js
@@ -4,6 +4,7 @@ var gaia = {
         var map = $('<div id="gaia"/>');
         $('#middle').html(map);
         $('#lateral, #menu').remove();
+        sys.comodo.hide();
         var inmap = $('<div class="inmap"/>');
         map.html(inmap);
         inmap.html(sys.loading(60, 'div').css({marginTop: 50}));
