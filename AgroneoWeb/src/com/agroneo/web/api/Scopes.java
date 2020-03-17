4e8a7d1c318adcf477db609b776ba65748fa17ba
pagetronic <pagetronic@gmail.com>
2020-03-17 15:35:21 +0400

    simplify

--- AgroneoWeb/src/com/agroneo/web/api/Scopes.java
+++ AgroneoWeb/src/com/agroneo/web/api/Scopes.java
@@ -1,16 +1,11 @@
 package com.agroneo.web.api;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import java.util.Arrays;
 
 @WebListener
-public class Scopes implements ServletContextListener {
-
-
-	@Override
-	public void contextInitialized(ServletContextEvent sce) {
+public class Scopes {
+	public Scopes() {
 		live.page.web.api.Scopes.scopes.addAll(Arrays.asList(
 				"gaia",
 				"email",
@@ -18,10 +13,5 @@ public class Scopes implements ServletContextListener {
 				"threads",
 				"accounts"
 		));
-	}
-
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-
 	}
 }
